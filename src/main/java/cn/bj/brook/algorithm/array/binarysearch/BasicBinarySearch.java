7c389f651a5d6098706338959605da21a05cef98
lanjie911 <911.chen@163.com>
2020-04-12 09:30:46 +0800

    优化二分法查找

--- src/main/java/cn/bj/brook/algorithm/array/binarysearch/BasicBinarySearch.java
+++ src/main/java/cn/bj/brook/algorithm/array/binarysearch/BasicBinarySearch.java
@@ -1,54 +1,51 @@
 package cn.bj.brook.algorithm.array.binarysearch;
 
+import cn.bj.brook.algorithm.sort.SortUtil;
+
 /**
  * 基本二分法查找元素
  */
 public class BasicBinarySearch {
     /**
      * 思路是二分法
+     *
      * @param nums
      * @param target
      * @return
      */
     public int search(int[] nums, int target) {
-
-        if(nums.length == 1){
-            return nums[0] == target?0:-1;
+        if (nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
         }
-
         int left_pointer = 0;
-        int right_pointer = nums.length-1;
+        int right_pointer = nums.length - 1;
         int middle = -1;
-
-        while(left_pointer < right_pointer){
-
-            // 处理相邻两个元素无限循环
-            // 找不到返回-1
-            if(left_pointer+1 == right_pointer){
-                if(nums[left_pointer] == target){
-                    return left_pointer;
-                }
-                if(nums[right_pointer] == target){
-                    return right_pointer;
-                }
-                return -1;
-            }
-
-            middle = (left_pointer+right_pointer) / 2;
+        // 两个指针至少相隔2以上的距离
+        while (left_pointer + 1 < right_pointer) {
+            middle = (left_pointer + right_pointer) / 2;
             int middle_value = nums[middle];
-            // 目标值比中间值还大
-            if(target > middle_value){
+            if (target > middle_value) {
                 left_pointer = middle;
-                continue;
-            }
-            if(target < middle_value){
+            } else if (target < middle_value) {
                 right_pointer = middle;
-                continue;
+            } else {
+                return middle;
             }
-
-            break;
-
         }
-        return middle;
+        // 最后处理数组中指针相邻的情况
+        if (nums[left_pointer] == target) {
+            return left_pointer;
+        }
+        if (nums[right_pointer] == target) {
+            return right_pointer;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,3,5,7,9};
+        BasicBinarySearch search = new BasicBinarySearch();
+        int t = search.search(arr, 5);
+        System.out.println("t="+t);
     }
 }
